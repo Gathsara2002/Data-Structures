d94cc5920602f0e012aaf8aef6ed59810de70c9b
Gathsara2002 <liviru2002mmg@gmail.com>
2024-01-28 11:53:05 +0530

    Added array ds

---
+++ Data-Structutes/src/Array/ArrayDemo.java
@@ -0,0 +1,28 @@
+package Array;
+
+/**
+ * @author : Gathsara
+ * created : 1/28/2024 -- 11:20 AM
+ **/
+
+//same data type store karaganna use karana container ekak
+//fixed length ekak tiyenne
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        // Declaration and Initialization
+        int[] arr = new int[5]; // Creating an integer array of size 5
+
+        // Accessing elements
+        arr[0] = 10; // Assigning value 10 to the first element
+        int value = arr[2]; // Accessing the third element
+        System.out.println("value: " + value);
+
+        //Iterating array
+        for (int i : arr) {
+            System.out.println(i);
+        }
+
+    }
+}
